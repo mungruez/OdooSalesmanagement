2797c2b1c14f54cc367b803a3d8be27550ee5dea
Zaakir Mungrue <mungruez@gmail.com>
2024-07-27 13:58:48 -0400

    Update sales_order.py

--- basic_sales_management/models/sales_order.py
+++ basic_sales_management/models/sales_order.py
@@ -4,6 +4,6 @@ class ShopSalesOrder(models.Model):
     _name = 'shop.sales.order'
     _description = 'Sales Order Record'
 
-    order_date = fields.Date(string='Name', required=True,  track_visibility="always")
     customer_id = fields.Many2one('shop.customer', string='Customer ID')
+    order_date = fields.Date(string='Name', required=True,  track_visibility="always")
     sales_order_lines = fields.One2many('shop.sales.order.line', 'sales_order_line', string='Sales Order Lines')
