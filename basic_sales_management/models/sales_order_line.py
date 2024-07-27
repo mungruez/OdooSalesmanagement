3b22220d95907ecb19d7d36c90d33d2092ff4748
Zaakir Mungrue <mungruez@gmail.com>
2024-07-27 13:59:26 -0400

    Update sales_order_line.py

--- basic_sales_management/models/sales_order_line.py
+++ basic_sales_management/models/sales_order_line.py
@@ -5,6 +5,6 @@ class ShopSalesOrderLine(models.Model):
     _name = 'shop.sales.order.line'
     _description = 'Sales Order Line'
 
-    product_id = fields.Many2one('shop.product', string='Product ID')
     product_quantity = fields.Integer(string="Quantity")
+    product_id = fields.Many2one('shop.product', string='Product ID')
     sales_order_line = fields.Many2One('shop.sales.order', string='Sales Order')
