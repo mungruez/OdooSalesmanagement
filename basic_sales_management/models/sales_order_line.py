d3107b1b4d622291105fc66659f3b0c78af44ddf
Zaakir Mungrue <mungruez@gmail.com>
2024-08-04 14:31:31 -0400

    Update sales_order_line.py

--- basic_sales_management/models/sales_order_line.py
+++ basic_sales_management/models/sales_order_line.py
@@ -1,9 +1,10 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 class ShopSalesOrderLine(models.Model):
     _name = 'shop.sales.order.line'
     _description = 'Sales Order Line'
 
-    product_id = fields.Many2one('shop.product', string='Product ID')
-    product_quantity = fields.Integer(string="Quantity")
-    sales_order_line = fields.Many2one('shop.sales.order', string='Sales Order')
+    product_quantity = fields.Integer(string="Quantity", required=True, track_visibility="always")
+    product_id = fields.Many2one(comodel_name='shop.product', string='Product ID', required=True)
+    sales_order_line = fields.Many2one(comodel_name='shop.sales.order', string='Sales Order', required=True)
+
