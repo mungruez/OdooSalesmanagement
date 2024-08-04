b0fada028ad56501a46126d25eb479e7e96bac17
Zaakir Mungrue <mungruez@gmail.com>
2024-08-04 14:32:24 -0400

    Update __manifest__.py

--- basic_sales_management/__manifest__.py
+++ basic_sales_management/__manifest__.py
@@ -1,14 +1,16 @@
 {
     'name':    'Shop Management System',
     'author':  'Zaakir Mungrue',
+    'version': '1.0',
     'website': 'https://mungruez.github.io/myPortfolioWebsite/',
-    'summary': 'Create and manage sales orders, customers and products',
+    'summary': 'Create and manage Customers, Products and Sales Orders',
     'depends': [],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
         'views/customer.xml',
-        'views/sales_order.xml',
         'views/product.xml',
+        'views/sales_order.xml',
+        'views/sales_order_line.xml',
     ],
 }
