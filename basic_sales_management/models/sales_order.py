adfbf96e97217c325ec58ecf1b8554700b10226f
Zaakir Mungrue <mungruez@gmail.com>
2024-08-04 14:30:38 -0400

    Update sales_order.py

--- basic_sales_management/models/sales_order.py
+++ basic_sales_management/models/sales_order.py
@@ -1,9 +1,9 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 class ShopSalesOrder(models.Model):
     _name = 'shop.sales.order'
     _description = 'Sales Order Record'
 
-    customer_id = fields.Many2one('shop.customer', string='Customer ID')
-    order_date = fields.Date(string='Name', required=True,  track_visibility="always")
+    order_date = fields.Datetime(string="Order Date", required=True, default=fields.Datetime.now, track_visibility="always")
+    customer_id = fields.Many2one(comodel_name='shop.customer', string='Customer ID', required=True)
     sales_order_lines = fields.One2many('shop.sales.order.line', 'sales_order_line', string='Sales Order Lines')
