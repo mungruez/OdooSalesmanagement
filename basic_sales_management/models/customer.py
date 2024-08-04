02bb6fb678d32b5c62a906175b5a92ba56e3935d
Zaakir Mungrue <mungruez@gmail.com>
2024-08-04 14:29:33 -0400

    Update customer.py

--- basic_sales_management/models/customer.py
+++ basic_sales_management/models/customer.py
@@ -1,9 +1,9 @@
-from odoo import models, fields
+from odoo import api,models, fields
 
 class ShopCustomer(models.Model):
     _name = 'shop.customer'
     _description = 'Customer Records'
-
+    
+    customer_email = fields.Char(string="Customer Email", required=True)
     sales_order = fields.One2many('shop.sales.order', 'customer_id', string='Customer ID')
-    customer_email = fields.Char(string="Email")
    
