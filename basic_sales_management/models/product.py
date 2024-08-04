ce49df253b00f6ef3f5ae49cf3d1d045fc513751
Zaakir Mungrue <mungruez@gmail.com>
2024-08-04 14:30:05 -0400

    Update product.py

--- basic_sales_management/models/product.py
+++ basic_sales_management/models/product.py
@@ -1,9 +1,9 @@
-from odoo import models, fields
+from odoo import api, models, fields
 
 class ShopProduct(models.Model):
     _name = 'shop.product'
     _description = 'Product Record'
     
-    product_name = fields.Char(string='Name', required=True,  track_visibility="always")
-    product_price = fields.Float(string="Price")
+    product_name = fields.Char(string='Name', required=True, track_visibility="always")
+    product_price = fields.Float(string="Price", required=True, track_visibility="always")
     sales_order_line = fields.One2many('shop.sales.order.line', 'product_id', string='Sales Order')
